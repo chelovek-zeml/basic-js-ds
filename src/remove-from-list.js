f66695c9135e340f1a41eaf4cd96cd6e3fc2716b
chelovek-zeml <stranger37561@gmail.com>
2022-10-17 23:10:14 +0300

    feat: add remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value == k) l = l.next; 
+
+  let origin = l;
+  let bef = origin;
+
+  while (origin) {
+    
+    if (origin.value == k) {
+      bef.next = origin.next;
+    } else {
+      bef = origin;
+    }
+    origin = origin.next;
+  }
+
+  return l;
 }
 
 module.exports = {
